86385dd5fd9bfdc93e91c42cb1f6ff5d203ea2ef
n0ks-0155 <4t3w35g@gmail.com>
2025-06-30 18:15:04 +1000

    utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,6 @@
-# app/utils.py
 import re
 
 def extract_objects(text: str):
-    # Разделяем по союзам и знакам препинания
     parts = re.split(r'[и,.\-]', text.lower())
     objects = [p.strip() for p in parts if len(p.strip()) > 3]
     return objects
